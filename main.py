b7370aacce6d54086eaef3d71965caf2e4e5d6ee
David Cacuango <66538886+David20022502@users.noreply.github.com>
2021-06-12 01:21:11 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+
+palabras ={}
+def iniciarContador():
+  archivo= open('texto.txt','r')
+  linea = archivo.readline()
+  lineas = linea.split()
+  while linea!="":
+     for palabraa in lineas:
+      if palabraa in palabras:
+        x=int(palabras[palabraa])
+        palabras[palabraa]=x+1
+      else:
+        palabras[palabraa]=1
+     linea = archivo.readline()
+     lineas = linea.split()
+  archivo.close()
+
+def imprimir():
+  for a in palabras:
+    num = str(palabras[a])
+    print(a+"= "+num)
+
+iniciarContador()
+print("Las palabras repetidas son ")
+imprimir()
